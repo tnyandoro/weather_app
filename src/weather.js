48020f790de3e9fb752f557056c7b3e618b5ca87
Tendai Nyandoro <tnyandoro@gmail.com>
2021-03-28 01:43:45 +0200

    Get API to collect weather data

---
+++ src/weather.js
@@ -0,0 +1,21 @@
+let weather = {
+   "apiKey": "fc8448ecbdfb7ff960c2d74a2a385fdc",
+    fetchWeather: function (city) {
+      fetch(
+         "http://api.openweathermap.org/data/2.5/weather?q="
+         + city 
+         +"&appid=" 
+         + this.apiKey
+         ).then((response) => response.json())
+         .then((data) => this.displayWeather(data));
+
+   },
+   displayWeather: function(data){
+    const { name } = data
+    const { icon, description } = data.weather; 
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    const { all } = data.clouds;
+    console.log(name, icon, description, temp, humidity, speed, all);
+   }
+}
--- src/weather.js.js
+++
@@ -1,3 +0,0 @@
-let weather = {
-   " apiKey": "fc8448ecbdfb7ff960c2d74a2a385fdc"
-}
