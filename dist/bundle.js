ad756eed2ab6aa6089208464212aa96b8a7fedef
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-16 21:06:54 +0200

    Fix the mode

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,54 @@
-(()=>{var e={apiKey:"fc8448ecbdfb7ff960c2d74a2a385fdc",fetchWeather:function(e){var t=this;fetch("http://api.openweathermap.org/data/2.5/weather?q=".concat(e,"&units=metric&appid=").concat(this.apiKey)).then((function(e){return e.json()})).then((function(e){return t.displayWeather(e)}))},displayWeather:function(e){var t=e.name,n=e.weather[0],c=n.icon,r=n.description,a=e.main,o=a.temp,i=a.humidity,u=e.wind.speed;document.querySelector(".city").innerText="Weather in ".concat(t),document.querySelector(".icon").src="https://openweathermap.org/img/wn/".concat(c,".png"),document.querySelector(".description").innerText=r,document.querySelector(".temp").innerText="".concat(o,"°C"),document.querySelector(".humidity").innerText="Humidity: ".concat(i,"%"),document.querySelector(".wind").innerText="Wind speed: ".concat(u," km/h"),document.querySelector(".weather").classList.remove("fetching"),document.body.style.backgroundImage="url('https://source.unsplash.com/random/1920x1080/?".concat(t,"')")},search:function(){this.fetchWeather(document.querySelector(".search-bar").value)}};document.querySelector(".search button").addEventListener("click",(function(){e.search()})),document.querySelector(".search-bar").addEventListener("keyup",(function(t){"Enter"===t.key&&e.search()})),e.fetchWeather("Harare")})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var weather = {
+  apiKey: 'fc8448ecbdfb7ff960c2d74a2a385fdc',
+  fetchWeather: function fetchWeather(city) {
+    var _this = this;
+
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=".concat(city, "&units=metric&appid=").concat(this.apiKey)).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return _this.displayWeather(data);
+    });
+  },
+  displayWeather: function displayWeather(data) {
+    var name = data.name;
+    var _data$weather$ = data.weather[0],
+        icon = _data$weather$.icon,
+        description = _data$weather$.description;
+    var _data$main = data.main,
+        temp = _data$main.temp,
+        humidity = _data$main.humidity;
+    var speed = data.wind.speed; //  const { all } = data.clouds;
+    // console.log(name, icon, description, temp, humidity, speed);
+
+    document.querySelector('.city').innerText = "Weather in ".concat(name);
+    document.querySelector('.icon').src = "https://openweathermap.org/img/wn/".concat(icon, ".png");
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = "".concat(temp, "\xB0C");
+    document.querySelector('.humidity').innerText = "Humidity: ".concat(humidity, "%");
+    document.querySelector('.wind').innerText = "Wind speed: ".concat(speed, " km/h");
+    document.querySelector('.weather').classList.remove('fetching');
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080/?".concat(name, "')");
+  },
+  // Search function for
+  search: function search() {
+    this.fetchWeather(document.querySelector('.search-bar').value);
+  }
+}; // Events
+
+document.querySelector('.search button').addEventListener('click', function () {
+  weather.search();
+});
+document.querySelector('.search-bar').addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    weather.search();
+  }
+});
+weather.fetchWeather('Harare');
+/******/ })()
+;
 //# sourceMappingURL=bundle.js.map
